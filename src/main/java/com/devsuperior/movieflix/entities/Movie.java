6976e5f758b72ced5a412de54333e084f1c16c71
Leo Abrantes <leoabrantesmelo@gmail.com>
2023-06-22 20:17:19 -0300

    Changing Movie

--- src/main/java/com/devsuperior/movieflix/entities/Movie.java
+++ src/main/java/com/devsuperior/movieflix/entities/Movie.java
@@ -5,13 +5,12 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
@@ -28,12 +27,12 @@ public class Movie implements Serializable {
 	private String subTitle;
 	private Integer year;
 	private String imgUrl;
+	
+	@Column(columnDefinition = "TEXT")
 	private String synopsis;
 	
-	@ManyToOne(fetch = FetchType.EAGER)
-	@JoinTable(name = "tb_movie_genre",
-		joinColumns = @JoinColumn(name = "movie_id"),
-		inverseJoinColumns = @JoinColumn(name = "genre_id"))	
+	@ManyToOne
+	@JoinColumn(name = "genre_id")	
 	private Genre genre;
 	
 	@OneToMany(mappedBy = "movie")
