3afe6141d9ab16efab9a25fef51ea4b873350a16
Leo Abrantes <leoabrantesmelo@gmail.com>
2023-07-12 13:14:12 -0300

    Creating ReviewDTO

--- src/main/java/com/devsuperior/movieflix/dto/ReviewDTO.java
+++ src/main/java/com/devsuperior/movieflix/dto/ReviewDTO.java
@@ -2,31 +2,65 @@ package com.devsuperior.movieflix.dto;
 
 import java.io.Serializable;
 
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-
-import com.devsuperior.movieflix.entities.Movie;
-import com.devsuperior.movieflix.entities.User;
+import com.devsuperior.movieflix.entities.Review;
 
 public class ReviewDTO implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
 	private Long id;
 	private String text;
-	private User user;
+	private UserDTO userDTO;
+	private MovieDTO movieDTO;
 	
 	public ReviewDTO() {
 	}
 	
-	public ReviewDTO(Long id, String title, String subTitle, Integer year, String imgUrl) {
+	public ReviewDTO(Long id, String text, UserDTO userDTO, MovieDTO movieDTO) {
 		this.id = id;
-
+		this.text = text;
+		this.userDTO = userDTO;
+		this.movieDTO = movieDTO;
 	}
 	
-	public ReviewDTO(Movie entity) {
+	public ReviewDTO(Review entity) {
 		id = entity.getId();
+		text = entity.getText();
+		userDTO = new UserDTO(entity.getUser());
+		movieDTO = new MovieDTO(entity.getMovie());
 
 	}
 
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	public UserDTO getUserDTO() {
+		return userDTO;
+	}
+
+	public void setUserDTO(UserDTO userDTO) {
+		this.userDTO = userDTO;
+	}
+
+	public MovieDTO getMovieDTO() {
+		return movieDTO;
+	}
+
+	public void setMovieDTO(MovieDTO movieDTO) {
+		this.movieDTO = movieDTO;
+	}
+	
 	
 }
